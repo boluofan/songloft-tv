f5748462fa94cd5ec7ccbce1144a6df2a0479644
Boluofan <13588211351@163.com>
2026-08-02 16:41:22 +0800

    chore: release version 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.songloft.tv"
         minSdk = 21
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.1.1"
+        versionCode = 6
+        versionName = "1.1.2"
     }
 
     val keystorePath = System.getenv("ANDROID_KEYSTORE_PATH")
